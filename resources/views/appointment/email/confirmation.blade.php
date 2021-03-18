b2ec5beb65a6e25c1b9c084f60803f73e02f99f7
Kees van Opdorp <keesjrvanopdorp@live.nl>
2021-03-18 14:05:58 +0100

    changed the basic send email

--- resources/views/appointment/email/confirmation.blade.php
+++ resources/views/appointment/email/confirmation.blade.php
@@ -1,7 +1,11 @@
 @component('mail::message')
 	<p>
         Beste meneer of mevrouw {{$appointment->user->name}},<br>
-        datum: {{$appointment->date}}
+        bedankt voor het boeken van een afspraak met Kees van Opdorp. <br>
+        Hieronder vindt u de details: <br>
+        datum: {{ $appointment->date }} <br>
+        reden: {{ $appointment->reason }} <br>
+        beschrijving: {{ $appointment->description }} <br>
     </p>
 Thanks,<br>
 {{ config('app.name') }}
